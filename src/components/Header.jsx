ea738e12ce0dac06cd746cdaa37118bed732b42d
ALI-RAZA816 <razadeveloper816@gmail.com>
2026-03-10 19:55:44 -0700

    complete header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,19 @@
+import { FaWallet } from "react-icons/fa";
+import { AiOutlineStock } from "react-icons/ai";
+import { MdSavings } from "react-icons/md";
+import style from '../css/Header.module.css';
+
+
+export default function Header() {
+  return (
+    <header className={style.header}>
+        <div className={style.left}>
+            <h1><FaWallet className={style.wallet} style={{color:"#00C689",fontSize:"2.3rem"}} /> WealthWise</h1>
+        </div>
+        <div className={style.right}>
+            <button><AiOutlineStock style={{marginRight:".5rem",color:"#7C6AE4",fontSize:"1.2rem"}}  /> <span>Smart Budgeting</span></button>
+            <button><MdSavings style={{marginRight:".5rem",color:"#7C6AE4",fontSize:"1.2rem"}} /><span>Save More</span></button>
+        </div>
+    </header>
+  )
+}
