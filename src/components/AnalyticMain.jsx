5ed7bb4a3e6eb5da49ace9915f815e563353ad93
ALI-RAZA816 <razadeveloper816@gmail.com>
2026-03-11 10:14:48 -0700

    styling forms component

--- src/components/AnalyticMain.jsx
+++ src/components/AnalyticMain.jsx
@@ -1,27 +1,14 @@
 import style from '../css/AnalyticMain.module.css';
 import { FaArrowUp, FaArrowDown } from "react-icons/fa";
 import { FaScaleBalanced } from "react-icons/fa6";
-import { Pie, PieChart, Tooltip} from 'recharts';
+import { Pie, PieChart, Tooltip, Legend} from 'recharts';
+import { FaPlus } from "react-icons/fa6";
 
 const data01 = [
   { name: 'Group A', value: 400 },
   { name: 'Group B', value: 300 },
   { name: 'Group C', value: 300 },
   { name: 'Group D', value: 200 },
-];
-
-const data02 = [
-  { name: 'A1', value: 100 },
-  { name: 'A2', value: 300 },
-  { name: 'B1', value: 100 },
-  { name: 'B2', value: 80 },
-  { name: 'B3', value: 40 },
-  { name: 'B4', value: 30 },
-  { name: 'B5', value: 50 },
-  { name: 'C1', value: 100 },
-  { name: 'C2', value: 200 },
-  { name: 'D1', value: 150 },
-  { name: 'D2', value: 50 },
 ];
 
 const isAnimationActive = true;
@@ -63,9 +50,9 @@ export default function AnalyticMain() {
                         <button>Income vs Expense</button>
                     </div>
                 </div>
-                <div className="actualChart">
+                <div className={style.actualChart}>
                     <PieChart
-                        style={{ width: '100%', height: '100%', maxWidth: '500px', maxHeight: '80vh', aspectRatio: 1 }}
+                        style={{ width: '100%', height: '100%', maxWidth: '500px', maxHeight: '35vh', aspectRatio: 1 }}
                         responsive
                         >
                         <Pie
@@ -73,28 +60,51 @@ export default function AnalyticMain() {
                             dataKey="value"
                             cx="50%"
                             cy="50%"
-                            outerRadius="50%"
-                            fill="#8884d8"
-                            isAnimationActive={isAnimationActive}
-                        />
-                        <Pie
-                            data={data02}
-                            dataKey="value"
-                            cx="50%"
-                            cy="50%"
-                            innerRadius="60%"
+                            innerRadius="50%"
                             outerRadius="80%"
                             fill="#82ca9d"
-                            label
+                            // label
                             isAnimationActive={isAnimationActive}
                         />
+                        <Legend/>
                         <Tooltip defaultIndex={defaultIndex} />
-
                     </PieChart>
                 </div>
             </div>
       </div>
-      <div className="right" style={{border:"1px solid red"}}></div>
+      <div className={style.right} style={{border:"1px solid red"}}>
+        <div className={style.transaction}>
+            <h2>Add Transaction</h2>
+            <form action="">
+                <input type="text" placeholder='Description' />
+                <input type="number" placeholder='Amount' />
+                <div className={style.selection}>
+                    <div>
+                        <select name="" id="">
+                            <option selected value="Expense">Expense</option>
+                            <option value="Income">Income</option>
+                        </select>
+                    </div>
+                    <div>
+                        <select name="" id="">
+                            <option selected disabled value="">Select catag</option>
+                            <option value="Housing">Housing</option>
+                            <option value="Transporation">Transporation</option>
+                            <option value="Food">Food</option>
+                            <option value="Utilities">Utilities</option>
+                            <option value="Entertainment">Entertainment</option>
+                            <option value="Healthcare">Healthcare</option>
+                            <option value="Shopping">Shopping</option>
+                            <option value="Education">Eduction</option>
+                            <option value="Other">Other</option>
+                        </select>
+                    </div>
+                </div>
+                <button type='submit'><FaPlus /><span>Add Transaction</span></button>
+            </form>
+        </div>
+        <div className="filter"></div>
+      </div>
     </div>
   )
 }
