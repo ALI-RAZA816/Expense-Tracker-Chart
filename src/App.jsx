9eca0da73faffb2a36a29bd4db943d16742d30ec
ALI-RAZA816 <razadeveloper816@gmail.com>
2026-03-11 15:29:00 -0700

    saperate the components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import './App.css';
 import Header from './components/Header';
 // import recharts from "recharts";
-import AnalyticMain from './components/AnalyticMain';
+// import AnalyticMain from './components/AnalyticMain';
 import Cards from './components/Cards';
 import ChartContainer from './components/ChartContainer';
 import FormSidebar from './components/FormSidebar';
